231d47b62219346519ed5f9ff16be0eba5e12b06
Wofluzz <matthiasviand@gmail.com>
2024-07-24 18:17:54 +0200

    Health System

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    private void TakeDamage(float _damage)
+    {
+
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            //Player Hurt
+        }
+        else
+        {
+            //Player Died
+        }
+    }
+}
